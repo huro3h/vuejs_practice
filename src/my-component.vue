446e2d4ab69f80619d0582cf0663a7070961b664
huro3h <huro3h@gmail.com>
2021-05-02 14:41:08 +0900

    sample login form component

--- src/my-component.vue
+++ src/my-component.vue
@@ -1,10 +1,31 @@
 <template>
-  <div>test!</div>
+  <div>
+    <input type="text" placeholder="enter your email" v-model="email">
+    <input type="text" placeholder="password" v-model="password">
+    <button @click="login">ログイン</button>
+  </div>
 </template>
 
 <script>
+const auth = {
+  login: function(email, pass) {
+    console.log(email + pass);
+  }
+}
+
 export default {
-  name: "my-component.vue"
+  name: "my-component.vue",
+  data: function () {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+  methods: {
+    login: function () {
+      auth.login(this.email, this.password)
+    }
+  }
 }
 </script>
 
