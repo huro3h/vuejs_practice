e72bbcf1f762621a581bb5ed7047c88747108081
huro3h <huro3h@gmail.com>
2021-05-03 13:29:36 +0900

    add routes

--- vuejs_practice_router/src/router/index.js
+++ vuejs_practice_router/src/router/index.js
@@ -1,15 +1,31 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+// import Top from '@/components/Top'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
+    // {
+    //   path: '/',
+    //   name: 'HelloWorld',
+    //   component: HelloWorld
+    // },
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      // name: 'Top',
+      component: {
+        template: '<div>Topページですね</div>'
+      }
+    },
+    {
+      path: '/users',
+      // name: 'Top',
+      component: {
+        template: '<div>ユーザー一覧ページですね</div>'
+      }
+    },
     }
   ]
 })
