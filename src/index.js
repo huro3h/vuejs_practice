e99180a362756fea4154f942c8971a7c346668af
huro3h <huro3h@gmail.com>
2021-05-03 12:51:58 +0900

    removed files

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import * as Vue from 'vue';
-import MyComponent from './my-component.vue';
-
-new Vue({
-  el: '#are-list',
-  components: { MyComponent },
-  template: '<my-component></my-component>'
-})
