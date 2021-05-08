665b6964dbeb85c95b27cfb6b798874403834878
huro3h <huro3h@gmail.com>
2021-05-08 14:18:54 +0900

    Add login, logout routes and function

---
+++ vuejs_practice_router/src/components/Login.vue
@@ -0,0 +1,68 @@
+<template>
+  <div>
+    <h3>Login</h3>
+    <p v-if="$route.query.redirect">
+      ログインしてください
+    </p>
+    <form @submit.prevent="login">
+      <label><input v-model="email" placeholder="email"></label>
+      <label><input v-model="pass" placeholder="password" type="password"></label>
+      <br>
+      <button type="submit">ログイン</button>
+      <p v-if="error" class="error">ログインに失敗しましたね..</p>
+    </form>
+  </div>
+</template>
+
+<script>
+const Auth = {
+  login: function (email, pass, cb) {
+    // ダミーデータを使った疑似ログイン
+    setTimeout(function () {
+      if (email === 'vue@example.com' && pass === 'vue') {
+        // ログイン成功時はローカルストレージにtokenを保存
+        localStorage.token = Math.random().toString(36).substring(7)
+        if (cb) { cb(true) }
+      } else {
+        if (cb) { cb(false) }
+      }
+    }, 400)
+  },
+
+  logout: function () {
+    delete localStorage.token
+  },
+
+  loggedIn: function () {
+    // ローカルストレージにtokenがあればログイン状態とみなす
+    return !!localStorage.token
+  }
+}
+
+export default {
+  name: "Login",
+  data: function () {
+    return {
+      email: 'vue@example.com',
+      pass: '',
+      error: false
+    }
+  },
+  methods: {
+    login: function () {
+      Auth.login(this.email, this.pass, (function (loggedIn) {
+        if (!loggedIn) {
+          this.error = true
+        } else {
+          // redirectパラメータがついている時は指定されたパスに遷移
+          this.$router.replace(this.$route.query.redirect || '/')
+        }
+      }).bind(this))
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- vuejs_practice_router/src/router/index.js
+++ vuejs_practice_router/src/router/index.js
@@ -4,6 +4,7 @@ import Top from '@/components/Top'
 import Users from '@/components/Users'
 import UserDetail from '@/components/UserDetail'
 import UserCreate from '@/components/UserCreate'
+import Login from '@/components/Login'
 
 Vue.use(Router)
 
@@ -24,12 +25,35 @@ export default new Router({
     {
       path: '/users/new',
       component: UserCreate,
+      beforeEnter: function (to, from, next) {
+        // 認証されていない時はloginページへ
+        if (!Auth.loggedIn()) {
+          next({
+            path: 'login',
+            query: { redirect: to.fullPath }
+          })
+        } else {
+          // 認証済みであれば、新規ユーザー登録ページへ
+          next()
+        }
+      }
     },
     {
       // ユーザID部分はコロンではじまるパターンマッチング
       path: '/users/:userId',
       // name: 'UserIds',
       component: UserDetail,
+    },
+    {
+      path: '/login',
+      component: Login,
+    },
+    {
+      path: '/logout',
+      beforeEnter: function (to, from, next) {
+        Auth.logout()
+        next('/')
+      }
     },
     {
       path: '*',
