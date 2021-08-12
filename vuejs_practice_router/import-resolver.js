76234be23f5d336c05581acfb44b7ccef7086b76
huro3h <huro3h@gmail.com>
2021-08-12 14:36:23 +0900

    pre reinit

--- vuejs_practice_router/import-resolver.js
+++
@@ -1,8 +0,0 @@
-// r.f. https://youtrack.jetbrains.com/issue/WEB-22717#focus=streamItem-27-1558931-0-0
-// r.f. https://qiita.com/IsaoTakahashi/items/20c82de0ddd2b71f4f75#import%E6%96%87%E3%81%AEwarning%E8%A7%A3%E6%B1%BA
-
-System.config({
-  paths: {
-    "@/*": "src/*"
-  }
-});
