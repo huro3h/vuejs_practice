e338bb1a80e6a57c841bbbc49686d1eee94ff83f
huro3h <huro3h@gmail.com>
2021-08-14 11:56:43 +0900

    v-if v-else

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,14 +1,19 @@
 <template>
-  <my-name></my-name>
+  <div>
+    <my-name></my-name>
+    <dog-form></dog-form>
+  </div>
 </template>
 
 <script>
 import Myname from "./Myname";
+import DogForm from "./DogForm";
 
 export default {
   name: "App",
   components: {
     'my-name': Myname,
+    'dog-form': DogForm,
   },
   
 }
---
+++ src/components/DogForm.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <p>DogForm</p>
+    <form>
+      <label for="toggle-show">ON / OFF ボタン</label>
+      <input type="checkbox" id="toggle-show" v-model="panelToggle">
+    </form>
+    <div id="panel" v-if="panelToggle">
+      見える?
+    </div>
+    <div v-else>
+      見えてないよ
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DogForm.vue",
+  data() {
+    return {
+      panelToggle: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
