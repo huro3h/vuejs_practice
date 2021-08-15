9a105f47cf99ce0211a88f5654278673ed6a6c80
huro3h <huro3h@gmail.com>
2021-08-15 17:06:06 +0900

    emit : 親コンポーネント: 定義 子コンポーネント:  送る準備

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,24 +1,28 @@
 <template>
   <div>
     <my-name :huga="hugaString"></my-name>
-    <dog-form></dog-form>
+    <!--    <dog-form></dog-form>-->
+    <my-counter :counter="AreCounter"></my-counter>
   </div>
 </template>
 
 <script>
 import MyName from "./MyName";
 import DogForm from "./DogForm";
+import MyCounter from "./MyCounter";
 
 export default {
   name: "App",
   data() {
     return {
-      hugaString: 'ふが'
+      hugaString: 'ふが',
+      AreCounter: 0,
     }
   },
   components: {
     'my-name': MyName,
-    'dog-form': DogForm,
+    // 'dog-form': DogForm,
+    'my-counter': MyCounter,
   },
   
 }
---
+++ src/components/MyCounter.vue
@@ -0,0 +1,23 @@
+<template>
+  <input type="button" @click="onclick" v-bind:value="counter">
+</template>
+
+<script>
+export default {
+  name: "MyCounter",
+  props: [
+    'counter'
+  ],
+  // 子 -> 親へ送るイベント
+  emits: ['increment'],
+  methods: {
+    onclick() {
+      this.$emit('increment', Number(this.counter))
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
